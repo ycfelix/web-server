7c51ceb4987cd889f08b73c447e4ff7641108384
vgf604999 <ycfelix7@gmail.com>
2020-06-23 18:13:39 +0800

    scp forget to add idrsa

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 tar -zcvf webserver.tar.gz .
-scp webserver.tar.gz ubuntu@18.217.87.1:/home/ubuntu
+scp -i id_rsa webserver.tar.gz ubuntu@18.217.87.1:/home/ubuntu
 
 #Here you extract your archive on EC2 using SSH
 echo "Start deploy"
-ssh -i id_rsa ubuntu@18.217.87.1 tar -xvzf webserver.tar.gz && java -jar target/webserver-1.0.jar
+ssh -i id_rsa ubuntu@18.217.87.1 
+tar -xvzf webserver.tar.gz
+java -jar target/webserver-1.0.jar
 echo "Deploy end"

6ae4ab641aa8b034503bdcc6fd01f2025e641417
vgf604999 <ycfelix7@gmail.com>
2020-06-23 18:07:02 +0800

    try again

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ scp webserver.tar.gz ubuntu@18.217.87.1:/home/ubuntu
 
 #Here you extract your archive on EC2 using SSH
 echo "Start deploy"
-yes yes | ssh -i id_rsa ubuntu@18.217.87.1 tar -xvzf webserver.tar.gz && java -jar target/webserver-1.0.jar
+ssh -i id_rsa ubuntu@18.217.87.1 tar -xvzf webserver.tar.gz && java -jar target/webserver-1.0.jar
 echo "Deploy end"

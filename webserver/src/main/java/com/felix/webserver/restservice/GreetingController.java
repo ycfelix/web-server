faacc8fde644070b69fee332c6f8dd560fcb50e2
vgf604999 <ycfelix7@gmail.com>
2020-06-23 13:12:56 +0800

    spring boot simple web server

---
+++ webserver/src/main/java/com/felix/webserver/restservice/GreetingController.java
@@ -0,0 +1,20 @@
+package com.felix.webserver.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong id = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")
+                                     String name) {
+        return new Greeting(id.incrementAndGet(), String.format(template, name));
+    }
+}

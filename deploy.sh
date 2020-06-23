07549d3ed7af6b6ded4c1d7a4c3ca463ab3168f4
vgf604999 <ycfelix7@gmail.com>
2020-06-23 16:51:13 +0800

    use new method

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+tar -zcvf webserver.tar.gz .
+scp webserver.tar.gz ubuntu@18.217.87.1:/home/ubuntu
+
+#Here you extract your archive on EC2 using SSH
+ssh ubuntu@18.217.87.1 tar -xvzf webserver.tar.gz
 echo "Start deploy"
-cd ~/circleci-aws
-git pull
 java -jar target/webserver-1.0.jar
 echo "Deploy end"

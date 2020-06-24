a81b16559f98ae024ed8ac74d9b333d4fbf67418
vgf604999 <ycfelix7@gmail.com>
2020-06-24 10:41:50 +0800

    update ssh script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-
+#
+#  ssh + scp without storing or prompting for keys.
+#
+function sshtmp
+{
+    ssh -o "ConnectTimeout 3" \
+        -o "StrictHostKeyChecking no" \
+        -o "UserKnownHostsFile /dev/null" \
+            "$@"
+}
+function scptmp
+{
+    exec scp -o "ConnectTimeout 3" \
+        -o "StrictHostKeyChecking no" \
+        -o "UserKnownHostsFile /dev/null" \
+        "$@"
+}
 tar -zcvf webserver.tar.gz .
 chmod 400 id_rsa
-scp -i id_rsa webserver.tar.gz ubuntu@18.217.87.1:/home/ubuntu
+scptmp -i id_rsa webserver.tar.gz ubuntu@18.217.87.1:/home/ubuntu
 
 #Here you extract your archive on EC2 using SSH
 echo "Start deploy"
-ssh -i id_rsa ubuntu@18.217.87.1 
+sshtmp -i id_rsa ubuntu@18.217.87.1 
 tar -xvzf webserver.tar.gz
 java -jar target/webserver-1.0.jar
 echo "Deploy end"

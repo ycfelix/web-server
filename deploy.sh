5fae4f403d00be050fa523dcfa1cbdbf17431b4e
vgf604999 <ycfelix7@gmail.com>
2020-06-24 11:24:52 +0800

    should work

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,5 @@ echo "Start deploy"
 ssh -i id_rsa -o "ConnectTimeout 3" \
         -o "StrictHostKeyChecking no" \
         -o "UserKnownHostsFile /dev/null" \
-            "$@" ubuntu@18.217.87.1  'tar -xvzf webserver.tar.gz && java -jar target/webserver-1.0.jar'
+            "$@" ubuntu@18.217.87.1  'tar -xvzf webserver.tar.gz && exec java -jar target/webserver-1.0.jar'
 echo "Deploy end"
